d808b3ed608587daa413104ee85ceb46885b687c
Rishindu Yohan <rishinduyohan@gmail.com>
2026-01-13 11:40:45 +0530

    create user entity and DTO classes

---
+++ src/main/java/edu/icet/task/model/entity/User.java
@@ -0,0 +1,33 @@
+package edu.icet.task.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "imageUrl")
+    private String imageUrl;
+}
